4a407e45e97ffd919ddb70390e970e40ca0b8b15
Nikhil Nayak <nikhil.nixel@gmail.com>
2021-05-17 21:46:49 +0530

    update with logger

--- api/dist/main.js
+++ api/dist/main.js
@@ -3,9 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const config_1 = require("@nestjs/config");
+const morgan = require("morgan");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const configService = app.get(config_1.ConfigService);
+    app.use(morgan('tiny'));
     app.setGlobalPrefix('api');
     await app.listen(configService.get('http.port'));
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
+  app.use(morgan('tiny')); // Logger
   app.setGlobalPrefix('api');
   await app.listen(configService.get('http.port'));
 }
